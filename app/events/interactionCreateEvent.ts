9127a8d2d7a380e2984179add742121439163643
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-05-29 20:23:59 +0200

    Create interactionCreateEvent.ts

---
+++ app/events/interactionCreateEvent.ts
@@ -0,0 +1,50 @@
+import { Events } from "discord.js";
+import { logs } from "../functions/logs";
+import { commandDraft } from "./interaction/command/commandDraft";
+
+export const interactionCreateEventInit = (client: any) => {
+  client.on(Events.InteractionCreate, async (interaction: any) => {
+    if (interaction.isButton()) {
+      //Buttons
+      try {
+      } catch (error: any) {
+        logs("error", "interaction:slash_command", error);
+      }
+    }
+
+    if (interaction.isChatInputCommand()) {
+      // Commands
+      try {
+        commandDraft(client, interaction);
+      } catch (error: any) {
+        logs("error", "interaction:slash_command", error);
+      }
+    }
+
+    if (interaction.isUserContextMenuCommand()) {
+      // Context user
+      try {
+      } catch (error: any) {
+        logs("error", "interaction:slash_command", error);
+      }
+    }
+
+    if (interaction.isMessageContextMenuCommand()) {
+      // Context message
+      try {
+      } catch (error: any) {
+        logs("error", "interaction:slash_command", error);
+      }
+    }
+
+    if (interaction.isModalSubmit()) {
+      // Modal
+      try {
+      } catch (error: any) {
+        logs("error", "interaction:slash_command", error);
+      }
+    }
+
+    return;
+  });
+};
